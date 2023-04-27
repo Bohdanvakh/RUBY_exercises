738011bf30fd497d7efef0e0b6cbc1f3b6430f64
Bohdan Vakh <bohdanvah@gmail.com>
2023-04-27 14:02:32 +0300

    task 5 completed

--- ruby_exercises.rb
+++ ruby_exercises.rb
@@ -21,19 +21,19 @@ def task_3___string_n_copy(string, n)
   output = ""
 
   n.times do |i|
-    output += string * (i+1)
-    output += "\n" unless i == n-1
+    output += string * (i+1) + "\n"
   end
 
   puts output
 end
 
-#task_3___string_n_copy("a", 5)
+#task_3___string_n_copy("abo", 5)
 
 def task_4___compute_circle_parameter(radius)
   pi = Math::PI
-  radious_value = "Input the radius of the circle: The perimeter is " + radius.to_s + "."
   area = radius ** 2 * pi
+
+  radious_value = "Input the radius of the circle: The perimeter is " + radius.to_s + "."
   area_value = "The area is " + area.to_s + "."
 
   #return radious_value, area_value
@@ -42,3 +42,9 @@ def task_4___compute_circle_parameter(radius)
 end
 
 #task_4___compute_circle_parameter(31.41592653)
+
+def task_5___check_string_start(strign)
+  puts strign.start_with?("if")
+end
+
+task_5___check_string_start("something")
