eeaba7f78a2bcc678171f6416de2ed7f5de51126
Bohdan Vakh <bohdanvah@gmail.com>
2023-04-27 12:40:42 +0300

    added_first_task

---
+++ ruby_exercises.rb
@@ -0,0 +1,9 @@
+def tasl_1___ruby_version()
+  version = RUBY_VERSION
+  patch_level = RUBY_PATCHLEVEL
+
+  puts "Ruby Version: #{version}"
+  puts "Ruby Version: #{patch_level}"
+end
+
+tasl_1___ruby_version
