731a7e1e78cafa42d1453e81c1e4f8ed3891d45f
Bohdan Vakh <bohdanvah@gmail.com>
2023-04-27 13:25:04 +0300

    tasks 3 and 4 completed

--- ruby_exercises.rb
+++ ruby_exercises.rb
@@ -6,7 +6,7 @@ def tasl_1___ruby_version()
   puts "Ruby Version: #{patch_level}"
 end
 
-tasl_1___ruby_version
+#tasl_1___ruby_version
 
 def task_2___date_time()
   date = Time.new
@@ -15,4 +15,30 @@ def task_2___date_time()
   puts "Current Date and Time: " + date_format
 end
 
-task_2___date_time
+#task_2___date_time
+
+def task_3___string_n_copy(string, n)
+  output = ""
+
+  n.times do |i|
+    output += string * (i+1)
+    output += "\n" unless i == n-1
+  end
+
+  puts output
+end
+
+#task_3___string_n_copy("a", 5)
+
+def task_4___compute_circle_parameter(radius)
+  pi = Math::PI
+  radious_value = "Input the radius of the circle: The perimeter is " + radius.to_s + "."
+  area = radius ** 2 * pi
+  area_value = "The area is " + area.to_s + "."
+
+  #return radious_value, area_value
+  puts radious_value
+  puts area_value
+end
+
+#task_4___compute_circle_parameter(31.41592653)
